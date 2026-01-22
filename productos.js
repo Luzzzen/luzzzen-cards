562b0e76e939dc4c690d7900ffaa216e9d25df0a
Luzzzen <nazarenocarreto@gmail.com>
2026-01-22 12:11:47 -0300

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,18 @@
+const productos = [
+  {
+    nombre: "Hazard - Chelsea – Panini WCCF 2016",
+    precio: 7500,
+    imagenes: [
+      "imagenes/HazardPruebaFront.jpg",
+      "imagenes/HazardPruebaBack.jpg"
+    ]
+  },
+  {
+    nombre: "Didier Drogba – Panini Colección 2016",
+    precio: 5000,
+    imagenes: [
+      "imagenes/HazardPruebaFront.jpg",
+      "imagenes/HazardPruebaBack.jpg"
+    ]
+  }
+];
